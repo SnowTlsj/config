e43540914700e27155389ee1e325c99b3252ff27
小呱呱～ <94552711+zhiguagua@users.noreply.github.com>
2021-11-26 13:03:10 +0800

    Update 必读.sh

--- 必读.sh
+++ 必读.sh
@@ -3,30 +3,18 @@ https://blog.csdn.net/z_162423/article/details/121212649
 或者ql目录下新建一个deps文件夹，将魔改版jdCookie.js拖入config和deps
 
 
-#2.10.8版本及以下自行在before文件末尾新添以下十行
-# 在实际执行任务前，确保集合仓库的脚本目录中的jdCookie.js是修改版的内容
-echo 开始复制魔改版jdCookie.js ...
-cp /ql/config/jdCookie.js /ql/scripts/shufflewzc_faker2/jdCookie.js
-cp /ql/config/jdCookie.js /ql/scripts/cdle_carry/jdCookie.js
-
-# 在这里加上你实际的仓库，比如说是xxx/yyy这个仓库，则是
-# cp /ql/config/jdCookie.js /ql/scripts/xxx_yyy/jdCookie.js
-
-echo 复制完毕
-
-
-
-#第二
+#第一
 新建定时任务“初始化任务”
 ql extra
 拉取仓库修复依赖
 
-#第三 注意去code文件调节互助模式，绿色文字注释好好看
-新建定时任务“格式化更新助力码”
-task /ql/config/code.sh（code.sh存放在config目录下）
-或者
+#第二 注意去code文件调节互助模式，绿色文字注释好好看
+新建定时任务“格式化更新助力码”---容器一
+task /ql/config/code.sh（code.sh存放在/ql/config目录下）
+
+或者-----容器二（修改code.sh后放入/ql1/scripts目录下）
 task code.sh（code.sh文件存放在scripts目录下）
-调节内部互助
+调节内部互助，可超出45ck限制
 
 
 删除多余文件例如config目录下的.git
